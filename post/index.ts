0c38870c98bd4c9705d9adc80d7efdfa3b7ae353
Petr Juna <petr.juna@gmail.com>
2019-09-15 13:47:30 +0200

    fix(user+post): lint fix

--- post/index.ts
+++ post/index.ts
@@ -2,4 +2,3 @@ export { CreatePostDto } from './create-post.dto';
 export { IPostModel } from './post.model';
 export { PostStateEnum } from './post-state.enum';
 export { ISectionModel } from './section.model';
-
