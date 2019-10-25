b1b0f20d295985fe300a8260fec7969006f58219
Petr Juna <petr.juna@gmail.com>
2019-10-25 22:42:38 +0200

    fix(create-auth): mock added

--- src/auth/create-auth.model.ts
+++ src/auth/create-auth.model.ts
@@ -1,6 +1,13 @@
 import { CreateEntityAbstract } from '../create-entity.abstract';
 
 export class CreateAuthModel extends CreateEntityAbstract {
+  public static MOCK_PROPERTIES = {
+    userId: 'abc',
+    token: 'abc',
+  };
+
+  public static MOCK = new CreateAuthModel(CreateAuthModel.MOCK_PROPERTIES);
+
   public readonly userId!: string;
 
   public readonly token!: string;
