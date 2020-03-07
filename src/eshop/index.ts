734df14f7442b49913115aaac84c27598b7cd8c9
Petr Juna <petr.juna@gmail.com>
2020-03-07 18:40:33 +0100

    fix(model): export change

--- src/eshop/index.ts
+++ src/eshop/index.ts
@@ -2,4 +2,4 @@ export { ProductSchema, PRODUCT_MODEL, CreateProductModel, ProductModel } from '
 export * from './order';
 export * from './cart';
 export * from './category';
-export * from './payment';
+export { PaymentModel, StripeAcknowledgeReceiptModel } from './payment';
